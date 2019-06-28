6343286cf5ec96b2511685790a7118982057e5f3
SebRiq <sebastien.riquier@inserm.fr>
2019-06-28 16:17:35 +0200

    In case APPRIS give a transcript ID absent in the reference's fasta (outdated ID ?), the longer transcript is taken

--- kmerator.jl
+++ kmerator.jl
@@ -367,7 +367,7 @@ function HTTPTEST(url)
     res = String(r.body)
     res = JSON.Parser.parse(res)
     
-    println(res)
+    #println(res)
 
     return res
   catch err
@@ -467,6 +467,7 @@ end
 # WARNING work only with ensembl fasta descriptions!
 if unannotated_option == false
     genes_already_processed = []
+    genes_analysed = []
     FastaReader("$fastafile") do fr
     for (desc, seq) in fr
       desc_array = split(desc)
@@ -483,30 +484,50 @@ if unannotated_option == false
       if isempty(select_option) || (select_option != nothing && (gene_name in select_option || ensembl_transcript_name in select_option || ensembl_gene_name in select_option)) && (gene_name in genes_already_processed) == false
         # take all sequence corresponding to asked gene names (select option)
         # take all if select_option not provided
-	if APPRIS_option != nothing 
-	APPRIS_transcript = APPRIS_function(ensembl_gene_name) 
-          println("APPRIS selected variant : $APPRIS_transcript")
-        end
-        #find_longer_variant(gene_name,dico_transcriptome)
-        println("$ensembl_transcript_name")
-        println("$gene_name")
-        #println("$gene_name:$ensembl_transcript_name" == find_longer_variant(gene_name,dico_transcriptome))
-        if APPRIS_option == nothing || (APPRIS_option != nothing && ensembl_transcript_name == APPRIS_transcript) || (APPRIS_option != nothing && APPRIS_transcript == "NODATA" && "$gene_name:$ensembl_transcript_name" == find_longer_variant(gene_name,dico_transcriptome))
-      println("$ensembl_transcript_name")
-          if length("$seq") >= kmer_length 
-            println("$gene_name:$ensembl_transcript_name : good length, continue")
-            FastaWriter("$output/sequences/$kmer_length/$gene_name:$ensembl_transcript_name.fa") do fwsequence
-        #for (desc2, seq2) in fr
-            write(fwsequence, [">$gene_name:$ensembl_transcript_name", "$seq"])
-	    push!(genes_already_processed, gene_name)
+	  if APPRIS_option != nothing 
+	    APPRIS_transcript = APPRIS_function(ensembl_gene_name) 
+            println("APPRIS selected variant : $APPRIS_transcript")
+		push!(genes_analysed, gene_name)
           end
+        #find_longer_variant(gene_name,dico_transcriptome)
+          println("$ensembl_transcript_name")
+          println("marqueur ici : $gene_name")
+          #println("$gene_name:$ensembl_transcript_name" == find_longer_variant(gene_name,dico_transcriptome))
+	 
+          if APPRIS_option == nothing || (APPRIS_option != nothing && ensembl_transcript_name == APPRIS_transcript) || (APPRIS_option != nothing && APPRIS_transcript == "NODATA" && "$gene_name:$ensembl_transcript_name" == find_longer_variant(gene_name,dico_transcriptome)) 
+            println("ensembl_transcript_name : $ensembl_transcript_name")
+            if length("$seq") >= kmer_length 
+              println("$gene_name:$ensembl_transcript_name : good length, continue")
+              FastaWriter("$output/sequences/$kmer_length/$gene_name:$ensembl_transcript_name.fa") do fwsequence
+        #for (desc2, seq2) in fr
+              write(fwsequence, [">$gene_name:$ensembl_transcript_name", "$seq"])
+	      push!(genes_already_processed, gene_name)
+            end
       
-      else println("$gene_name:$ensembl_transcript_name : wrong length, ignored")
+          else println("$gene_name:$ensembl_transcript_name : wrong length, ignored")
+        end
       end
     end
-    end
-    end
-    end
+  end
+    println("genes analysed : $genes_analysed")
+    println("genes analysed (unique): $(unique(genes_analysed))")
+    for i in unique(genes_analysed)
+    	#println(" dollar i : $i")
+	#println("in genes already expressed : $(i in genes_already_processed)")
+   	# it can happen that APPRIS contain obscolete id or other error, in this case the longer variant is selected for the concerned genes (passed in loop but have not provided a "sequence" fasta)
+    	if (i in genes_already_processed) == false
+		println("ERROR : There is a problem with $i, APPRIS function have not worked properly, somtimes an outdated APPRIS reference, in this case the longer transcript is selected from the reference fasta")
+		id = find_longer_variant(i,dico_transcriptome)
+		#println("coucou")
+
+		#println(dico_transcriptome["$id"])
+              FastaWriter("$output/sequences/$kmer_length/$id") do fwsequence
+        	
+                write(fwsequence, [">$id", string(dico_transcriptome["$id"])])
+            end
+	  end
+	end
+end
   # if the input fasta is not a part of ensembl annotation (-u)
   else
     FastaReader("$fastafile") do fr
